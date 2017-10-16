0f2a4e2d7cbfa8d9d9a145e17d703da30db88f77
Domingo Oropeza <doropeza@teclib.com>
2017-10-16 12:15:23 -0400

    test(client): updated testing for client

--- tests/unit/Client.php
+++ tests/unit/Client.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace GlpiProject\API\Rest\tests\units;
+
 use atoum;
-use GuzzleHttp\Client as HttpClient;
 
 class Client extends atoum {
 
@@ -9,7 +10,7 @@ class Client extends atoum {
     * @engine inline
     */
    public function testInitSession() {
-      $client = $this->newTestedInstance(new HttpClient(), GLPI_URL);
+      $client = $this->newTestedInstance(GLPI_URL);
 
       // Test invalid credentials
       $this->exception(function() use ($client) {
@@ -25,11 +26,25 @@ class Client extends atoum {
     * @engine inline
     */
    public function testComputer() {
-      $client = $this->newTestedInstance(new HttpClient(), GLPI_URL);
+      $client = $this->newTestedInstance(GLPI_URL);
       $this->boolean($client->initSessionByCredentials('glpi', 'glpi'))->isTrue();
 
       $response = $client->computer('post', ['name' => 'computer 0001']);
       $this->object($response)->isInstanceOf(\Psr\Http\Message\ResponseInterface::class);
+   }
+
+   /**
+    * @engine inline
+    * @tags testGetFullSession
+    */
+   public function testGetFullSession() {
+      $client = $this->newTestedInstance(GLPI_URL);
+      $client->initSessionByCredentials('glpi', 'glpi');
+
+      $response = $client->getFullSession();
+      $this->array($response)
+         ->integer['statusCode']->isEqualTo(200)
+         ->string['body']->isNotEmpty()->given($json = $response['body'])->then->json($json);
    }
 
 }
