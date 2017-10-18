3bd5ae05297bd4598e38982de324b63bb31af023
Domingo Oropeza <doropeza@teclib.com>
2017-10-18 14:19:27 -0400

    docs(*): added GPL license to files

--- src/GlpiProject/API/Rest/Client.php
+++ src/GlpiProject/API/Rest/Client.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * LICENSE
+ *
+ * Copyright © 2017 Teclib'
+ *
+ * This file is part of GLPI Api Client Library for PHP.
+ *
+ * GLPI Api Client Library for PHP is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * GLPI Api Client Library for PHP is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * GLPI Api Client Library for PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with GLPI Api Client Library for PHP. If not, see http://www.gnu.org/licenses/.
+ * ------------------------------------------------------------------------------
+ * @author    Domingo Oropeza
+ * @copyright Copyright © 2017 Teclib
+ * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+ * @link      https://github.com/flyve-mdm/composer-package-glpi
+ * @link      https://flyve-mdm.com/
+ * ------------------------------------------------------------------------------
+ */
 
 namespace GlpiProject\API\Rest;
 
