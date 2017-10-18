80d8be0844adcbd4c74d227e1a88fd8977585801
Domingo Oropeza <doropeza@teclib.com>
2017-10-18 12:03:42 -0400

    test(endpoint): added new test to EndPointHandler class

--- tests/unit/EndPointHandler.php
+++ tests/unit/EndPointHandler.php
@@ -14,7 +14,7 @@ class EndPointHandler extends BaseTestCase {
    }
 
    public function tearDown() {
-      $this->client->killSession();
+//      $this->client->killSession();
    }
 
    /**
@@ -49,5 +49,70 @@ class EndPointHandler extends BaseTestCase {
       // assert invalid profile
       $this->array($response = $this->testedInstance->changeActiveProfile(-1));
       $this->assertJsonResponse($response, parent::HTTP_NOT_FOUND);
+   }
+
+   /**
+    * @tags testGetMyEntities
+    */
+   public function testGetMyEntities() {
+      $this->given($this->newTestedInstance($this->client));
+      $response = $this->testedInstance->getMyEntities();
+      $this->assertJsonResponse($response);
+
+      // The returned json has to had this key(s)
+      $stdClass = json_decode($response['body']);
+      $this->boolean(property_exists($stdClass,
+         'myentities'))->isTrue()->array($stdClass->myentities);
+
+      $response = $this->testedInstance->getMyEntities(['is_recursive' => true]);
+      $this->assertJsonResponse($response);
+      // TODO: complete the test for recursive cases.
+   }
+
+   /**
+    * @tags testGetActiveEntities
+    */
+   public function testGetActiveEntities() {
+      $this->given($this->newTestedInstance($this->client))
+         ->array($response = $this->testedInstance->getActiveEntities());
+      $this->assertJsonResponse($response);
+
+      // The returned json has to had this key(s)
+      $stdClass = json_decode($response['body']);
+      $this->boolean(property_exists($stdClass, 'active_entity'))->isTrue();
+      $this->boolean(property_exists($stdClass->active_entity,
+         'active_entity_recursive'))->isTrue();
+      $this->boolean(property_exists($stdClass->active_entity,
+         'active_entities'))->isTrue()->array($stdClass->active_entity->active_entities);
+   }
+
+   /**
+    * @tags testGetActiveEntities
+    */
+   public function testChangeActiveEntities() {
+      $this->given($this->newTestedInstance($this->client));
+      // check with no params (default behavior)
+      $this->array($response = $this->testedInstance->changeActiveEntities())
+         ->integer['statusCode']->isEqualTo(parent::HTTP_OK)
+         ->string['body']->isEqualTo('true');
+
+      // check for invalid entity param
+      $this->array($response = $this->testedInstance->changeActiveEntities(['entities_id'=> -1]))
+         ->integer['statusCode']->isEqualTo(parent::HTTP_OK)
+         ->string['body']->isEqualTo('false');
+
+      // check for invalid recursive param
+      $this->array($response = $this->testedInstance->changeActiveEntities(['is_recursive'=> "lorem"]))
+         ->integer['statusCode']->isEqualTo(parent::HTTP_OK)
+         ->string['body']->isEqualTo('true'); // this could be a bug from glpi.
+
+      $this->array($response = $this->testedInstance->changeActiveEntities(['entities_id'=> 0, 'is_recursive'=> "lorem"]))
+         ->integer['statusCode']->isEqualTo(parent::HTTP_OK)
+         ->string['body']->isEqualTo('true');
+
+      // check for valid params with different default values
+      $this->array($response = $this->testedInstance->changeActiveEntities(['entities_id'=> 0, 'is_recursive'=> true]))
+         ->integer['statusCode']->isEqualTo(parent::HTTP_OK)
+         ->string['body']->isEqualTo('true');
    }
 }
