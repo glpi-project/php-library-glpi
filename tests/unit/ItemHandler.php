85404509f829429abc9de563579727a950596602
Domingo Oropeza <doropeza@teclib.com>
2017-10-20 17:29:05 -0400

    docs(*): updated project license

--- tests/unit/ItemHandler.php
+++ tests/unit/ItemHandler.php
@@ -1,31 +1,28 @@
 <?php
 /**
+ * --------------------------------------------------------------------
+ *
  * LICENSE
  *
- * Copyright © 2017 Teclib'
+ * This file is part of the GLPI API Client Library for PHP,
+ * a subproject of GLPI. GLPI is a free IT Asset Management.
  *
- * This file is part of GLPI Api Client Library for PHP.
+ * GLPI is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
  *
- * GLPI Api Client Library for PHP is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
- *
- * GLPI Api Client Library for PHP is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * GLPI Api Client Library for PHP is distributed in the hope that it will be useful,
+ * GLPI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with GLPI Api Client Library for PHP. If not, see http://www.gnu.org/licenses/.
- * ------------------------------------------------------------------------------
- * @author    Domingo Oropeza
- * @copyright Copyright © 2017 Teclib
- * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
- * @link      https://github.com/flyve-mdm/composer-package-glpi
- * @link      https://flyve-mdm.com/
- * ------------------------------------------------------------------------------
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * --------------------------------------------------------------------
+ * @author    Domingo Oropeza - <doropeza@teclib.com>
+ * @copyright (C) 2017 Teclib' and contributors.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/glpi-project/php-library-glpi
+ * @link      http://www.glpi-project.org/
+ * --------------------------------------------------------------------
  */
 
 namespace Glpi\Api\Rest\tests\units;
