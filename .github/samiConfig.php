c5382372e6c494adfa241923aebdc26f6129ac7f
Domingo Oropeza <doropeza@teclib.com>
2017-11-13 11:31:27 -0400

    build(docs): added theme for documentation pages

--- .github/samiConfig.php
+++ .github/samiConfig.php
@@ -2,13 +2,19 @@
 
 use Sami\Version\GitVersionCollection;
 
-$dir = 'src';
-return new Sami\Sami($dir, [
-   'theme' => 'default',
+$dir = __DIR__ . '/../src';
+
+$options = [
+   'template_dirs' => [__DIR__ . '/docsTheme/'],
+   'theme' => 'teclib',
    'title' => 'GLPI API Client Library for PHP',
    'build_dir' => __DIR__ . '/../build/docs/%version%',
    'cache_dir' => __DIR__ . '/../build/cache/%version%',
    'simulate_namespaces' => true,
    'default_opened_level' => 1,
 //   'versions' => $versions,
-]);
+];
+
+$sami = new Sami\Sami($dir, $options);
+
+return $sami;
