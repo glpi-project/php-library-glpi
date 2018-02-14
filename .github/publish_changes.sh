237a9d8da4dee18e212a228ccfaec36948161504
Domingo Oropeza <doropeza@teclib.com>
2018-02-14 17:56:33 -0400

    ci(report): update script which generate reports
    Signed-off-by: Domingo Oropeza <doropeza@teclib.com>

--- .github/publish_changes.sh
+++ .github/publish_changes.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # please set the $GH_TOKEN in your travis dashboard
 
-if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_REPO_SLUG" = "glpi-project/php-library-glpi" ]; then
+if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
     wget http://get.sensiolabs.org/sami.phar -O "$HOME/bin/sami.phar"
     # setup_git only for the main repo and not forks
+    echo "Configuring git user"
     git config --global user.email "deploy@travis-ci.org"
     git config --global user.name "Deployment Bot"
+    echo "adding and fetch a new remote"
     git remote add upstream https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG".git > /dev/null 2>&1
     git fetch upstream
 
     if [ "$TRAVIS_BRANCH" = "develop" ]; then
+        echo "generating the release"
         git stash save -u
         vendor/bin/robo publish:release "$TRAVIS_REPO_SLUG" none upstream
         git checkout "$TRAVIS_BRANCH"
@@ -18,6 +21,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TR
 
     # check if gh-pages exist in remote
     if [ "git branch -r --list upstream/gh-pages" ]; then
+        echo "generating the docs"
         # clean the repo and generate the docs
         git checkout composer.lock
         php $HOME/bin/sami.phar update "$TRAVIS_BUILD_DIR"/.github/samiConfig.php --force
@@ -26,15 +30,18 @@ if [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TR
         find build/tests/coverage/ -type f -name "*.html" -exec sed -i "/report.css/d" "{}" \;
 
         # commit_website_files
+        echo "adding the reports"
         git add build/tests/coverage/*
         git add build/docs/*
+        echo "creating a branch for the new documents"
         git checkout -b localCi
         git commit -m "changes to be merged"
         git checkout -b gh-pages upstream/gh-pages
         git checkout localCi build/
 
         # upload_files
-        git commit --message "docs(*): update code coverage and test result"
+        echo "pushing the up to date documents"
+        git commit --message "docs: update tests reports"
         git rebase upstream/gh-pages
         git push --quiet --set-upstream upstream gh-pages --force
     fi
